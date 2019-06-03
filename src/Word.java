9efd6c0748e4f429120a5bbb581194f5494f1c86
Aditya Vikram Khandelwal <adityavkhandelwal@gmail.com>
2019-06-04 00:02:34 +1000

    Update Word.java

--- src/Word.java
+++ src/Word.java
@@ -16,7 +16,8 @@ public class Word implements Comparable <Word>, Serializable {
 		
 		for (int i = 0; i < wordLen; i++) {
 			charVal = Character.getNumericValue(word.charAt(i)) - 10; //getNumericValue returns between 10 and 35 (both inclusive)
-			if ((charVal >= 0) && (charVal <= 25))
+			if ((charVal >= 0) && (charVal <= 25)) //very important because users sometimes enter non-alphabetical characters
+							       //just to "test" the system
 				dbArray [charVal]++;
 		}
 	}
@@ -38,6 +39,13 @@ public class Word implements Comparable <Word>, Serializable {
 		}
 	}
 	
+	/********************************How this works? Lets compare aa and ab:***************************************\
+	For the first word, dbArray [0] is higher, so the word must come earlier in the lex ordering, hence we return -1.
+	Hence scanning across the dbArray of the two words A and B, if we find an entry dbArray [i] that is higher
+	in A, then A must come before in lex sequence. This idea is not wholly correct however, since aaa will be
+	placed before aa, or bcqqq will come before bcq by this logic. But this OK, since our method gives a comparator
+	that satisties the two key rules: (A < B => B > A) and vice versa and also (! (A < B) && ! (A > B) => (A == B))
+	\**************************************************************************************************************/
 	public int compareTo (Word other) {
 		if (other == null) 
 			throw new NullPointerException ();
